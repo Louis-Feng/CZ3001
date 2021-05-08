dbbd589312e2c8b7c9ea8179ad019b6475938a77
Louis-Feng <61575406+Louis-Feng@users.noreply.github.com>
2021-05-08 15:37:40 +0800

    Add files via upload

---
+++ Lab2/adder_test.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:56:19 01/27/2021
+// Design Name:   adder
+// Module Name:   C:/Users/feng0111/Desktop/Feng Qingyu/CE3001_lab1/adder_test.v
+// Project Name:  CE3001_lab1
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: adder
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module adder_test;
+
+	// Inputs
+	reg [63:0] a;
+	reg [63:0] b;
+
+	// Outputs
+	wire [63:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	adder uut (
+		.a(a), 
+		.b(b), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		#200 a=64'h01; //after 200ns make a=1;
+		#200 b=64'h02; // after 200ns make b=1;
+	end
+      
+endmodule
+
---
+++ Lab2/datapath_test.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:05:32 02/09/2021
+// Design Name:   datapath_mux
+// Module Name:   C:/Users/feng0111/Desktop/feng/lab2 codes/Feng Qingyu/Feng Qingyu/CE3001_lab1/datapath_test.v
+// Project Name:  CE3001_lab1
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: datapath_mux
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module datapath_test;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg [63:0] inst;
+
+	// Outputs
+	wire [63:0] aluout;
+
+	// Instantiate the Unit Under Test (UUT)
+	datapath_mux uut (
+		.clk(clk), 
+		.rst(rst), 
+		.inst(inst), 
+		.aluout(aluout)
+	);
+	always #15 clk = ~clk;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		inst = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		#10 rst =1;
+		#50 rst=0;
+		#50;// Wait 50 ns for global reset to finish
+		inst=32'b00000000000001000000000000100011;// ADD X3, X1, X4
+		// ISIZE =32, DSIZE=64 bit and Write and read ADDR= 5 bit. 
+		#100; inst=32'b00000000001000100000000000100100;//SUB X4,X1,X2
+	end
+      
+endmodule
+
